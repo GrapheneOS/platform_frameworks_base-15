GrapheneOS-Archive/platform_frameworks_base-old

AppBindArgs: infrastructure for passing extra args to app process init

services/core/java/com/android/server/ext/PackageManagerHooks.java
@@ -3,11 +3,16 @@ package com.android.server.ext;
 import android.Manifest;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
+import android.app.AppBindArgs;
 import android.content.pm.PackageManager;
+import android.os.Binder;
+import android.os.Bundle;
 import android.os.UserHandle;
 import android.util.ArraySet;
 
 import com.android.internal.util.GoogleEuicc;
+import com.android.server.pm.PackageManagerService;
+import com.android.server.pm.parsing.pkg.AndroidPackage;
 import com.android.server.pm.pkg.PackageStateInternal;
 import com.android.server.pm.pkg.parsing.ParsingPackage;
 
@@ -58,6 +63,29 @@ public class PackageManagerHooks {
         var set = new ArraySet<>(perms);
         pkg.getRequestedPermissions().removeAll(set);
         pkg.getUsesPermissions().removeIf(p -> set.contains(p.getName()));
+    }
+
+    @Nullable
+    public static Bundle getExtraAppBindArgs(PackageManagerService pm, String packageName) {
+        final int callingUid = Binder.getCallingUid();
+        final int appId = UserHandle.getAppId(callingUid);
+        final int userId = UserHandle.getUserId(callingUid);
+
+        AndroidPackage pkg = pm.snapshotComputer().getPackage(packageName);
+        if (pkg == null) {
+            return null;
+        }
+
+        if (pkg.getUid() != appId) { // getUid() actually returns appId, it has a historic name
+            return null;
+        }
+
+        int[] flagsArr = new int[AppBindArgs.FLAGS_ARRAY_LEN];
+
+        var b = new Bundle();
+        b.putIntArray(AppBindArgs.KEY_FLAGS_ARRAY, flagsArr);
+
+        return b;
     }
 
     // Called when AppsFilter decides whether to restrict package visibility

GrapheneOS/platform_frameworks_base-15

services/core/java/com/android/server/ext/PackageManagerHooks.java
@@ -3,9 +3,18 @@ package com.android.server.ext;
 import android.Manifest;
 import android.annotation.Nullable;
 import android.annotation.UserIdInt;
+import android.app.AppBindArgs;
+import android.content.pm.PackageManager;
 import android.content.pm.PackageManagerInternal;
+import android.os.Binder;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.UserHandle;
 import android.util.ArraySet;
 
+import com.android.server.pm.PackageManagerService;
+import com.android.server.pm.permission.Permission;
+import com.android.server.pm.pkg.AndroidPackage;
 import com.android.server.pm.pkg.PackageStateInternal;
 import com.android.server.pm.pkg.parsing.ParsingPackage;
 
@@ -24,6 +33,38 @@ public class PackageManagerHooks {
             PackageManagerInternal pm, String permName, String packageName, int userId)
     {
         return false;
+    }
+
+    @Nullable
+    public static Bundle getExtraAppBindArgs(PackageManagerService pm, String packageName) {
+        final int callingUid = Binder.getCallingUid();
+        final int appId = UserHandle.getAppId(callingUid);
+        final int userId = UserHandle.getUserId(callingUid);
+
+        PackageStateInternal pkgState = pm.snapshotComputer().getPackageStateInternal(packageName);
+        if (pkgState == null) {
+            return null;
+        }
+
+        if (pkgState.getAppId() != appId) {
+            return null;
+        }
+
+        AndroidPackage pkg = pkgState.getPkg();
+
+        if (pkg == null) {
+            return null;
+        }
+
+        // isSystem() remains true even if isUpdatedSystemApp() is true
+        final boolean isUserApp = !pkgState.isSystem();
+
+        int[] flagsArr = new int[AppBindArgs.FLAGS_ARRAY_LEN];
+
+        var b = new Bundle();
+        b.putIntArray(AppBindArgs.KEY_FLAGS_ARRAY, flagsArr);
+
+        return b;
     }
 
     // Called when AppsFilter decides whether to restrict package visibility