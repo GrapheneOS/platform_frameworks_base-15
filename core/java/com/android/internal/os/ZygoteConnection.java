GrapheneOS-Archive/platform_frameworks_base-old

infrastructure for passing extra args to Zygote and WebViewZygote

core/java/com/android/internal/os/ZygoteConnection.java
@@ -259,7 +259,7 @@ class ZygoteConnection {
                             parsedArgs.mInstructionSet, parsedArgs.mAppDataDir,
                             parsedArgs.mIsTopApp, parsedArgs.mPkgDataInfoList,
                             parsedArgs.mAllowlistedDataInfoList, parsedArgs.mBindMountAppDataDirs,
-                            parsedArgs.mBindMountAppStorageDirs);
+                            parsedArgs.mBindMountAppStorageDirs, parsedArgs.mExtraArgs);
 
                     try {
                         if (pid == 0) {

GrapheneOS/platform_frameworks_base-15

core/java/com/android/internal/os/ZygoteConnection.java
@@ -263,7 +263,7 @@ class ZygoteConnection {
                             parsedArgs.mIsTopApp, parsedArgs.mPkgDataInfoList,
                             parsedArgs.mAllowlistedDataInfoList, parsedArgs.mBindMountAppDataDirs,
                             parsedArgs.mBindMountAppStorageDirs,
-                            parsedArgs.mBindMountSyspropOverrides);
+                            parsedArgs.mBindMountSyspropOverrides, parsedArgs.mExtraArgs);
 
                     try {
                         if (pid == 0) {