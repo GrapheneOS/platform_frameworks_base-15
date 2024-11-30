GrapheneOS-Archive/platform_frameworks_base-old

SystemUI: require unlocking to use QS tiles by default

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -68,6 +68,7 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.SideLabelTileLayout;
 import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -267,6 +268,32 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      */
     public boolean isAvailable() {
         return true;
+    }
+
+    public enum Action {
+        CLICK,
+        SECONDARY_CLICK,
+        LONG_CLICK,
+    }
+
+    public boolean isAllowedWhenLocked(Action action) {
+        return false;
+    }
+
+    void handleAction(Action action, Runnable handler) {
+        if (isAllowedWhenLocked(action)) {
+            handler.run();
+            return;
+        }
+
+        KeyguardStateController ksc = mHost.getKeyguardStateController();
+        boolean hasSecureKeyguard = ksc.isMethodSecure() && ksc.isShowing();
+
+        if (hasSecureKeyguard) {
+            mActivityStarter.postQSRunnableDismissingKeyguard(handler);
+        } else {
+            handler.run();
+        }
     }
 
     // safe to call from any thread
@@ -592,16 +619,19 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                         mActivityStarter.postStartActivityDismissingKeyguard(intent, 0);
                     } else {
                         mQSLogger.logHandleClick(mTileSpec, msg.arg1);
-                        handleClick((View) msg.obj);
+                        View view = (View) msg.obj;
+                        handleAction(Action.CLICK, () -> handleClick(view));
                     }
                 } else if (msg.what == SECONDARY_CLICK) {
                     name = "handleSecondaryClick";
                     mQSLogger.logHandleSecondaryClick(mTileSpec, msg.arg1);
-                    handleSecondaryClick((View) msg.obj);
+                    View view = (View) msg.obj;
+                    handleAction(Action.SECONDARY_CLICK, () -> handleSecondaryClick(view));
                 } else if (msg.what == LONG_CLICK) {
                     name = "handleLongClick";
                     mQSLogger.logHandleLongClick(mTileSpec, msg.arg1);
-                    handleLongClick((View) msg.obj);
+                    View view = (View) msg.obj;
+                    handleAction(Action.LONG_CLICK, () -> handleLongClick(view));
                 } else if (msg.what == REFRESH_STATE) {
                     name = "handleRefreshState";
                     handleRefreshState(msg.obj);

GrapheneOS/platform_frameworks_base-15

packages/SystemUI/src/com/android/systemui/qs/tileimpl/QSTileImpl.java
@@ -69,6 +69,7 @@ import com.android.systemui.qs.QSHost;
 import com.android.systemui.qs.QsEventLogger;
 import com.android.systemui.qs.SideLabelTileLayout;
 import com.android.systemui.qs.logging.QSLogger;
+import com.android.systemui.statusbar.policy.KeyguardStateController;
 
 import java.io.PrintWriter;
 
@@ -267,6 +268,32 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
      */
     public boolean isAvailable() {
         return true;
+    }
+
+    public enum Action {
+        CLICK,
+        SECONDARY_CLICK,
+        LONG_CLICK,
+    }
+
+    public boolean isAllowedWhenLocked(Action action) {
+        return false;
+    }
+
+    void handleAction(Action action, Runnable handler) {
+        if (isAllowedWhenLocked(action)) {
+            handler.run();
+            return;
+        }
+
+        KeyguardStateController ksc = mHost.getKeyguardStateController();
+        boolean hasSecureKeyguard = ksc.isMethodSecure() && ksc.isShowing();
+
+        if (hasSecureKeyguard) {
+            mActivityStarter.postQSRunnableDismissingKeyguard(handler);
+        } else {
+            handler.run();
+        }
     }
 
     // safe to call from any thread
@@ -594,16 +621,19 @@ public abstract class QSTileImpl<TState extends State> implements QSTile, Lifecy
                         mActivityStarter.postStartActivityDismissingKeyguard(intent, 0);
                     } else {
                         mQSLogger.logHandleClick(mTileSpec, msg.arg1);
-                        handleClick((Expandable) msg.obj);
+                        Expandable view = (Expandable) msg.obj;
+                        handleAction(Action.CLICK, () -> handleClick(view));
                     }
                 } else if (msg.what == SECONDARY_CLICK) {
                     name = "handleSecondaryClick";
                     mQSLogger.logHandleSecondaryClick(mTileSpec, msg.arg1);
-                    handleSecondaryClick((Expandable) msg.obj);
+                    Expandable view = (Expandable) msg.obj;
+                    handleAction(Action.SECONDARY_CLICK, () -> handleSecondaryClick(view));
                 } else if (msg.what == LONG_CLICK) {
                     name = "handleLongClick";
                     mQSLogger.logHandleLongClick(mTileSpec, msg.arg1);
-                    handleLongClick((Expandable) msg.obj);
+                    Expandable view = (Expandable) msg.obj;
+                    handleAction(Action.LONG_CLICK, () -> handleLongClick(view));
                 } else if (msg.what == REFRESH_STATE) {
                     name = "handleRefreshState";
                     handleRefreshState(msg.obj);